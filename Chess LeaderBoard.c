fa4d532b302cf87f3191e1e78a62ae157789395c
ridwanRahin004 <92874151+ridwanRahin004@users.noreply.github.com>
2021-12-09 01:06:34 +0600

    Update Chess LeaderBoard.c

--- Chess LeaderBoard.c
+++ Chess LeaderBoard.c
@@ -1,91 +1,341 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-            long int match_number;
-            int match_played=4;
-            int ridwan_point=2;
-            int mohammad_point=2;
-            int first=1;
-            int second=1;
-            char ridwan[20]="RIDWAN";
-            char mohammad[20]="MOHAMMAD";
-        int result()
-        {
-            printf("\n\n\tLeader___Board");
-            printf("\n ___________");
-            printf("\n|Ridwan:   %d|" , ridwan_point);
-            printf("\n|Mohammad: %d|" , mohammad_point);
-            printf("\n'''''''''''''''");
-            //printf("\n\nTOP: %s" , ridwan );
-            //printf("\t%s" , mohammad);
-            //printf("\n\nTOP: %s" , ridwan );
-            //printf("\n\nTOP: %s" , mohammad );
-            if(ridwan_point>mohammad_point)
-            {
-                printf("\n\nTOP: %s\n\n\n\n" , ridwan );
-            }
-            else if(ridwan_point<mohammad_point)
-            {
-                printf("\n\nTOP: %s\n\n\n\n" , mohammad );
-            }
-            else{
-                printf("\n\nTOP: %s\tand" , ridwan );
-                printf("\t%s\n\n\n\n" , mohammad);
-            }
-        }
-int main()
+char ridwan_rank[10]="5";
+char mohammad_rank[10]="5";
+char p1 [20] ="Mohammad";
+char p2 [20] ="Ridwan";
+char ch[20]="Checkmate";
+char d1[20]="Undisputed";
+char d2[20]="Stalemate";
+char d3[25]="Dead Position";
+char d4[25]="Mutual Agreement";
+char d5[25]="Threefold Repetition";
+
+int just_match_number()
 {
-    int ler;
-    printf("\t\t\t###WELCOME###");
-    printf("\t\t\n\n%d Match played" , match_played);
-    printf("\n\nEnter Match Number: ");
-    scanf("%ld" , &match_number);
-    if(match_number==1)
+    int ma_num;
+    int go_back;
+    printf("\n\n\tEnter Match number: ");
+    scanf("%d" , &ma_num);
+    if(ma_num==1)
     {
-        printf("\n\tMatch Number\tWhite\tBalck  \t Result\t\t Point");
-        printf("\n\t1\t\tRidwan\tMohammad Mohammad Won\t 1");
-        printf("\n\nIf you want to see Leader___Board enter [1]");
-        scanf("%d" , &ler);
-        if(ler==1)
+        printf("\n\n\tMatch number: 1");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p1 ,ch);
+        printf("\n\tPoint: \tMohammad: 1\t\tRidwan: 0");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
         {
-            result();
+            main();
+        }
+        else{
+            just_match_number();
         }
     }
-    else if(match_number==2)
+    else if(ma_num==2)
     {
-        printf("\n\tMatch Number\tWhite\tBalck  \t Result\t\t Point");
-        printf("\n\t2\t\tMohammad\tMohammad Mohammad Won\t 1");
-        printf("\n\nIf you want to see Leader___Board enter [1]");
-        scanf("%d" , &ler);
-        if(ler==1)
+        printf("\n\n\tMatch number: 2");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: %s-WON By %s" , p2 ,ch);
+        printf("\n\tPoint: \tMohammad: 1\t\tRidwan: 1");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
         {
-            result();
+            main();
+        }
+        else{
+            just_match_number();
         }
     }
-    else if(match_number==3)
+    else if(ma_num==3)
     {
-        printf("\n\tMatch Number\tWhite\tBalck  \t Result\t\t Point");
-        printf("\n\t3\t\tMohammad\tMohammad Mohammad Won\t 1");
-        printf("\n\nIf you want to see Leader___Board enter [1]");
-        scanf("%d" , &ler);
-        if(ler==1)
+        printf("\n\n\tMatch number: 3");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: %s-WON By %s" , p1 ,ch);
+        printf("\n\tPoint: \tMohammad: 2\t\tRidwan: 1");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
         {
-            result();
+            main();
+        }
+        else{
+            just_match_number();
         }
     }
-    else if(match_number==4)
+    else if(ma_num==4)
     {
-        printf("\n\tMatch Number\tWhite\tBalck  \t Result\t\t Point");
-        printf("\n\t4\t\tRidwan\tMohammad Ridwan Won\t 1");
-        printf("\n\nIf you want to see Leader___Board enter [1]");
-        scanf("%d" , &ler);
-        if(ler==1)
+        printf("\n\n\tMatch number: 4");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p2 ,ch);
+        printf("\n\tPoint: \tMohammad: 2\t\tRidwan: 2");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
         {
-            result();
+            main();
+        }
+        else{
+            just_match_number();
+        }
+    }
+    else if(ma_num==5)
+    {
+        printf("\n\n\tMatch number: 5");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" ,d1);
+        printf("\n\tPoint: \tMohammad: 2.5\t\tRidwan: 2.5");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
+        {
+            main();
+        }
+        else{
+            just_match_number();
+        }
+    }
+    else if(ma_num==6)
+    {
+        printf("\n\n\tMatch number: 6");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p2 ,ch);
+        printf("\n\tPoint: \tMohammad: 2.5\t\tRidwan: 3.5");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
+        {
+            main();
+        }
+        else{
+            just_match_number();
+        }
+    }
+    else if(ma_num==7)
+    {
+        printf("\n\n\tMatch number: 7");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p1 ,ch);
+        printf("\n\tPoint: \tMohammad: 3.5\t\tRidwan: 3.5");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
+        {
+            main();
+        }
+        else{
+            just_match_number();
+        }
+    }
+    else if(ma_num==8)
+    {
+        printf("\n\n\tMatch number: 8");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" ,d2);
+        printf("\n\tPoint: \tMohammad: 4\t\tRidwan: 4");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
+        {
+            main();
+        }
+        else{
+            just_match_number();
+        }
+    }
+    else if(ma_num==9)
+    {
+        printf("\n\n\tMatch number: 9");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" ,d3);
+        printf("\n\tPoint: \tMohammad: 4.5\t\tRidwan: 4.5");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
+        {
+            main();
+        }
+        else{
+            just_match_number();
+        }
+    }
+    else if(ma_num==10)
+    {
+        printf("\n\n\tMatch Number: 10");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" , d3);
+        printf("\n\tPoint: \tMohammad: 5\t\tRidwan: 5");
+        printf("\n\nPress [1]: ");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
+        {
+            main();
+        }
+        else{
+            just_match_number();
         }
     }
     else{
-        printf("\n\n###Not Found###");
+        main();
+    }
+}
+
+int all_match_result()
+{
+    int go_back;
+    printf("\n\n\n Match played: 10");
+    printf("\n\t\t\tMohammad\n");
+    printf("\nWON: \tWhite: 1(match number 3)");
+    printf("\n     \tBlack: 2(match number 1,7)");
+    printf("\n\nLOST: \tWhite: 1(Match number 2)");
+    printf("\n      \tBlack: 2(Match number 4,6)");
+    printf("\n\nDRAW: \tWhite: 4(Match number 5,8,9,10)");
+    printf("\n      \tBlack: 0(Match number )");
+    printf("\n\n\n");
+    printf("\n\t\t\tRidwan\n");
+    printf("\nWON: \tWhite: 2(match number 4,6)");
+    printf("\n     \tBlack: 1(match number 2)");
+    printf("\n\nLOST: \tWhite: 2(Match number 1,7)");
+    printf("\n      \tBlack: 1(Match number 3)");
+    printf("\n\nDRAW: \tWhite: 0(Match number )");
+    printf("\n      \tBlack: 4(Match number 6,8,9,10)");
+    printf("\n\n\n\nPress [1]: ");
+    scanf("%d" , &go_back);
+    if(go_back==1)
+    {
+        main();
+    }
+}
+int leader_board()
+{
+    int go_back;
+    printf("\n\n\n\t\tMohammad: %s \t\t\tRidwan: %s" , mohammad_rank , ridwan_rank);
+    printf("\n\nPress [1]: ");
+    scanf("%d" , &go_back);
+    if(go_back==1)
+    {
+        main();
+    }
+}
+int match_number()
+{
+    int go_back;
+        printf("\n\n\tMatch number: 1");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p1 ,ch);
+        printf("\n\tPoint: \tMohammad: 1\t\tRidwan: 0");
+
+        printf("\n\n\tMatch number: 2");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: %s-WON By %s" , p2 ,ch);
+        printf("\n\tPoint: \tMohammad: 1\t\tRidwan: 1");
+
+        printf("\n\n\tMatch number: 3");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: %s-WON By %s" , p1 ,ch);
+        printf("\n\tPoint: \tMohammad: 2\t\tRidwan: 1");
+
+        printf("\n\n\tMatch number: 4");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p2 ,ch);
+        printf("\n\tPoint: \tMohammad: 2\t\tRidwan: 2");
+
+        printf("\n\n\tMatch number: 5");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" ,d1);
+        printf("\n\tPoint: \tMohammad: 2.5\t\tRidwan: 2.5");
+
+        printf("\n\n\tMatch number: 6");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p2 ,ch);
+        printf("\n\tPoint: \tMohammad: 2.5\t\tRidwan: 3.5");
+
+        printf("\n\n\tMatch number: 7");
+        printf("\n\tWhite: %s" , p2);
+        printf("\n\tBlack: %s" , p1);
+        printf("\n\tResult: %s-WON By %s" , p1 ,ch);
+        printf("\n\tPoint: \tMohammad: 3.5\t\tRidwan: 3.5");
+
+        printf("\n\n\tMatch number: 8");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" ,d2);
+        printf("\n\tPoint: \tMohammad: 4\t\tRidwan: 4");
+
+        printf("\n\n\tMatch number: 9");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" ,d3);
+        printf("\n\tPoint: \tMohammad: 4.5\t\tRidwan: 4.5");
+
+        printf("\n\n\tMatch Number: 10");
+        printf("\n\tWhite: %s" , p1);
+        printf("\n\tBlack: %s" , p2);
+        printf("\n\tResult: Draw By %s" , d3);
+        printf("\n\tPoint: \tMohammad: 5\t\tRidwan: 5");
+        printf("\n\nPress [1]: ");
+        scanf("%d" , &go_back);
+        if(go_back==1)
+        {
+            main();
+        }
+        else{
+            just_match_number();
+        }
+}
+int main()
+{
+    int menu_action;
+    printf("\t\t\tMohammad VS Ridwan");
+    printf("\n\t\t\t Match played(9)");
+    printf("\n\n\n 1.Leader board");
+    printf("\n 2.Match result");
+    printf("\n 3.All match result");
+    printf("\n 4.Player information");
+    printf("\n 5.Exit");
+    printf("\n  Enter Action: ");
+    scanf("%d" , &menu_action);
+    if(menu_action==1)
+    {
+        leader_board();
+    }
+    else if(menu_action==2)
+    {
+        just_match_number();
+    }
+    else if(menu_action==3)
+    {
+        match_number();
+    }
+    else if(menu_action==4)
+    {
+        all_match_result();
+    }
+    else if(menu_action==5)
+    {
+        printf("\t\t\t ##ByE##");
+    }
+    else{
         main();
     }
     return 0;
